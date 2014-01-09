fce98a91730f1a3ce4cfed7cb9078595890b6653
amcrn <amcreynolds@ebaysf.com>
2014-01-09 11:25:33 -0800

    Support Extensions to Trove Client
    extensions on the contrib path (i.e. troveclient/v1/contrib/*.py),
    extensions on the python path that follow the naming convention of
    *_python_troveclient_ext, and entry-points will now be discovered
    and loaded.
    
    due to the mismatch of the api version and the troveclient
    package name, '1.0' vs. '1', the client has been patched to look
    for contrib in troveclient/v1 vs. the standard troveclient/v1_0
    
    Change-Id: I3ea7bb5ba471b0d421581dcfda7216f429cb7b65
    Closes-Bug: #1267577

--- troveclient/utils.py
+++ troveclient/utils.py
@@ -247,6 +247,18 @@ class HookableMixin(object):
             hook_func(*args, **kwargs)
 
 
+def safe_issubclass(*args):
+    """Like issubclass, but will just return False if not a class."""
+
+    try:
+        if issubclass(*args):
+            return True
+    except TypeError:
+        pass
+
+    return False
+
+
 # http://code.activestate.com/recipes/
 #   577257-slugify-make-a-string-usable-in-a-url-or-filename/
 def slugify(value):
