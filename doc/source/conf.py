4a3615d8c51cd508e873ee94155da613f900678e
Andreas Jaeger <aj@suse.de>
2014-09-19 10:27:55 +0200

    Use OpenStack branding for trove client
    Add oslosphinx to get OpenStack branding.
    Remove html_static_path since it's unused and issues a warning.
    Update copyright year.
    
    Change-Id: I0bdcab8ebc5a201a3f91b4a6c4724536d1157901

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,7 +21,8 @@
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
-    'sphinx.ext.coverage'
+    'sphinx.ext.coverage',
+    'oslosphinx'
 ]
 
 templates_path = ['_templates']
@@ -31,14 +32,13 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 project = u'python-troveclient'
-copyright = u'2012, OpenStack Foundation'
+copyright = u'2014, OpenStack Foundation'
 
 exclude_trees = []
 
 pygments_style = 'sphinx'
 
 html_theme = 'default'
-html_static_path = ['_static']
 htmlhelp_basename = 'python-troveclientdoc'
 latex_documents = [
     ('index', 'python-troveclient.tex', u'python-troveclient Documentation',
