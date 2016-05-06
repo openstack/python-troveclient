7bb9b3c229fc75e5ea0347c08982823b4533fe48
Peter Stachowski <peter@tesora.com>
2016-05-06 12:23:31 -0400

    Persist error messages and display on 'show'
    Format the fault nicely on the Trove 'show' command.
    
    Change-Id: I92777ea410e8df21169fddcfdc0de1f5ca00e7bd
    Implements: blueprint persist-error-message

--- troveclient/v1/shell.py
+++ troveclient/v1/shell.py
@@ -88,6 +88,12 @@ def _print_instance(instance):
     if hasattr(instance, 'replicas'):
         replicas = [replica['id'] for replica in instance.replicas]
         info['replicas'] = ', '.join(replicas)
+    if hasattr(instance, 'fault'):
+        info.pop('fault', None)
+        info['fault'] = instance.fault['message']
+        info['fault_date'] = instance.fault['created']
+        if 'details' in instance.fault and instance.fault['details']:
+            info['fault_details'] = instance.fault['details']
     info.pop('links', None)
     utils.print_dict(info)
 
