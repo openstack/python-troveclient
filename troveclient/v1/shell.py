972beeafd079cc25fb0d3c1715b5666c0a7d8ed4
Masaki Matsushita <glass.saga@gmail.com>
2016-07-05 16:47:58 +0900

    Add vCPUs to flavor-list
    This commit adds vCPU colmn to flavor-list
    
    Closes-Bug: #1261876
    Change-Id: Id0ab43cdaa20918af278c44ea9bff7d23260e438

--- troveclient/v1/shell.py
+++ troveclient/v1/shell.py
@@ -220,8 +220,8 @@ def do_flavor_list(cs, args):
             f.id = f.str_id
         _flavors.append(f)
 
-    utils.print_list(_flavors, ['id', 'name', 'ram'],
-                     labels={'ram': 'RAM'})
+    utils.print_list(_flavors, ['id', 'name', 'ram', 'vcpus'],
+                     labels={'ram': 'RAM', 'vcpus': 'vCPUs'})
 
 
 @utils.arg('flavor', metavar='<flavor>', type=str,
