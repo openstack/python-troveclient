6a373890c360e37da29e616d97f6d276bbdf6d66
wangyao <wangyao@cmss.chinamobile.com>
2017-12-01 17:02:27 +0800

    Add flavor-show to OSC
    This change adds database support to the python-openstackclient
    project for the flavor-show command.
    
    The trove command flavor-show is now:
        openstack database flavor show
    
    Change-Id: Ie91f81bad239869bba21e1358cc97e8efa7bf468
    Partially-Implements: trove-support-in-python-openstackclient

--- troveclient/tests/osc/v1/test_database_flavors.py
+++ troveclient/tests/osc/v1/test_database_flavors.py
@@ -39,3 +39,26 @@ class TestFlavorList(TestFlavors):
         self.flavor_client.list.assert_called_once_with()
         self.assertEqual(self.columns, columns)
         self.assertEqual([self.values], values)
+
+
+class TestFlavorShow(TestFlavors):
+
+    values = (1, 'm1.tiny', 512)
+
+    def setUp(self):
+        super(TestFlavorShow, self).setUp()
+        self.cmd = database_flavors.ShowDatabaseFlavor(self.app, None)
+        self.data = self.fake_flavors.get_flavors_1()
+        self.flavor_client.get.return_value = self.data
+        self.columns = (
+            'id',
+            'name',
+            'ram',
+        )
+
+    def test_flavor_show_defaults(self):
+        args = ['m1.tiny']
+        parsed_args = self.check_parser(self.cmd, args, [])
+        columns, data = self.cmd.take_action(parsed_args)
+        self.assertEqual(self.columns, columns)
+        self.assertEqual(self.values, data)
