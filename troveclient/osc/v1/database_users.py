70a6d0e73d1c30e75b89ad1917337c766fb62b4d
zhanggang <zhanggang@cmss.chinamobile.com>
2017-12-08 00:02:13 -0500

    Add user-delete to OSC
    This change adds database support to the python-openstackclient
    project for the user-delete command.
    
    The trove command user-delete is now:
        openstack database user delete
    
    Change-Id: Ie11a2a41dfca0e1f58e155eaf7c5949e11352c6a
    Partially-Implements: trove-support-in-python-openstackclient

--- troveclient/osc/v1/database_users.py
+++ troveclient/osc/v1/database_users.py
@@ -13,6 +13,7 @@
 """Database v1 Users action implementations"""
 
 from osc_lib.command import command
+from osc_lib import exceptions
 from osc_lib import utils
 import six
 
@@ -75,3 +76,39 @@ class ShowDatabaseUser(command.ShowOne):
         user = db_users.get(parsed_args.instance, parsed_args.name,
                             hostname=parsed_args.host)
         return zip(*sorted(six.iteritems(user._info)))
+
+
+class DeleteDatabaseUser(command.Command):
+
+    _description = _("Deletes a user from an instance.")
+
+    def get_parser(self, prog_name):
+        parser = super(DeleteDatabaseUser, self).get_parser(prog_name)
+        parser.add_argument(
+            'instance',
+            metavar='<instance>',
+            help=_('ID or name of the instance.')
+        )
+        parser.add_argument(
+            'name',
+            metavar='<name>',
+            help=_('Name of user.')
+        )
+        parser.add_argument(
+            '--host',
+            metavar='<host>',
+            help=_('Optional host of user.')
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        manager = self.app.client_manager.database
+        users = manager.users
+        try:
+            instance = utils.find_resource(manager.instances,
+                                           parsed_args.instance)
+            users.delete(instance, parsed_args.name, parsed_args.host)
+        except Exception as e:
+            msg = (_("Failed to delete user %(user)s: %(e)s")
+                   % {'user': parsed_args.name, 'e': e})
+            raise exceptions.CommandError(msg)
