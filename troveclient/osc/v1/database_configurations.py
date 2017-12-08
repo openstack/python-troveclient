c2de9c6a2200f9935e3072bfd5abfac9ebc911da
wangyao <wangyao@cmss.chinamobile.com>
2017-12-08 11:01:26 +0800

    Add configuration-parameter-list to OSC
    This change adds database support to the python-openstackclient
    project for the configuration-parameter-list command.
    
    The trove command configuration-parameter-list is now:
        openstack database configuration parameter list
    
    Change-Id: I8e7ee36529b75b0962aeaf30a074b55b9e0cee12
    Partially-Implements: trove-support-in-python-openstackclient

--- troveclient/osc/v1/database_configurations.py
+++ troveclient/osc/v1/database_configurations.py
@@ -17,7 +17,9 @@ from osc_lib.command import command
 from osc_lib import utils as osc_utils
 import six
 
+from troveclient import exceptions
 from troveclient.i18n import _
+from troveclient import utils
 
 
 def set_attributes_for_print_detail(configuration):
@@ -80,3 +82,51 @@ class ShowDatabaseConfiguration(command.ShowOne):
             db_configurations, parsed_args.configuration_group)
         configuration = set_attributes_for_print_detail(configuration)
         return zip(*sorted(six.iteritems(configuration)))
+
+
+class ListDatabaseConfigurationParameters(command.Lister):
+
+    _description = _("Lists available parameters for a configuration group.")
+    columns = ['Name', 'Type', 'Min Size', 'Max Size', 'Restart Required']
+
+    def get_parser(self, prog_name):
+        parser = super(ListDatabaseConfigurationParameters, self).\
+            get_parser(prog_name)
+        parser.add_argument(
+            'datastore_version',
+            metavar='<datastore_version>',
+            help=_('Datastore version name or ID assigned'
+                   'to the configuration group.')
+        )
+        parser.add_argument(
+            '--datastore',
+            metavar='<datastore>',
+            default=None,
+            help=_('ID or name of the datastore to list configuration'
+                   'parameters for. Optional if the ID of the'
+                   'datastore_version is provided.')
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        db_configuration_parameters = self.app.client_manager.\
+            database.configuration_parameters
+        if parsed_args.datastore:
+            params = db_configuration_parameters.\
+                parameters(parsed_args.datastore,
+                           parsed_args.datastore_version)
+        elif utils.is_uuid_like(parsed_args.datastore_version):
+            params = db_configuration_parameters.\
+                parameters_by_version(parsed_args.datastore_version)
+        else:
+            raise exceptions.NoUniqueMatch(_('The datastore name or id is'
+                                             ' required to retrieve the'
+                                             ' parameters for the'
+                                             ' configuration group'
+                                             ' by name.'))
+        for param in params:
+            setattr(param, 'min_size', getattr(param, 'min', '-'))
+            setattr(param, 'max_size', getattr(param, 'max', '-'))
+        params = [osc_utils.get_item_properties(p, self.columns)
+                  for p in params]
+        return self.columns, params
