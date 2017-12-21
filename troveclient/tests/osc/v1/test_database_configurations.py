a1e9f46a53abd039b6be4ab7d7114d09f8722232
zhanggang <zhanggang@cmss.chinamobile.com>
2017-12-21 00:23:55 -0500

    Add configuration-delete to OSC
    This change adds database support to the python-openstackclient
    project for the configuration-delete command.
    
    The trove command configuration-delete is now:
        openstack database configuration delete
    
    Change-Id: I747566e5a1ddf415c3127a86debc2ba477256141
    Partially-Implements: trove-support-in-python-openstackclient

--- troveclient/tests/osc/v1/test_database_configurations.py
+++ troveclient/tests/osc/v1/test_database_configurations.py
@@ -10,6 +10,10 @@
 #   License for the specific language governing permissions and limitations
 #   under the License.
 
+import mock
+
+from osc_lib import utils
+
 from troveclient import common
 from troveclient import exceptions
 from troveclient.osc.v1 import database_configurations
@@ -167,5 +171,32 @@ class TestConfigurationParameterShow(TestConfigurations):
         ]
         parsed_args = self.check_parser(self.cmd, args, verifylist)
         self.assertRaises(exceptions.NoUniqueMatch,
+                          self.cmd.take_action,
+                          parsed_args)
+
+
+class TestDatabaseConfigurationDelete(TestConfigurations):
+
+    def setUp(self):
+        super(TestDatabaseConfigurationDelete, self).setUp()
+        self.cmd = database_configurations.\
+            DeleteDatabaseConfiguration(self.app, None)
+
+    @mock.patch.object(utils, 'find_resource')
+    def test_configuration_delete(self, mock_find):
+        args = ['config1']
+        mock_find.return_value = args[0]
+        parsed_args = self.check_parser(self.cmd, args, [])
+        result = self.cmd.take_action(parsed_args)
+        self.configuration_client.delete.assert_called_with('config1')
+        self.assertIsNone(result)
+
+    @mock.patch.object(utils, 'find_resource')
+    def test_configuration_delete_with_exception(self, mock_find):
+        args = ['fakeconfig']
+        parsed_args = self.check_parser(self.cmd, args, [])
+
+        mock_find.side_effect = exceptions.CommandError
+        self.assertRaises(exceptions.CommandError,
                           self.cmd.take_action,
                           parsed_args)
