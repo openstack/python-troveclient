84e5873a03791e53621fcbacfe69337a3627bdb3
zhanggang <zhanggang@cmss.chinamobile.com>
2017-12-23 04:54:54 -0500

    Add backup-list-instance to OSC
    This change adds database support to the python-openstackclient
    project for the backup-list-instance command.
    
    The trove command backup-list-instance is now:
        openstack database backup list instance
    
    Change-Id: I383cfe0bac54051641fe0589057c24e7d885f5c1
    Partially-Implements: blueprint trove-support-in-python-openstackclient

--- troveclient/tests/osc/v1/test_database_backups.py
+++ troveclient/tests/osc/v1/test_database_backups.py
@@ -27,6 +27,7 @@ class TestBackups(fakes.TestDatabasev1):
         super(TestBackups, self).setUp()
         self.mock_client = self.app.client_manager.database
         self.backup_client = self.app.client_manager.database.backups
+        self.instance_client = self.app.client_manager.database.instances
 
 
 class TestBackupList(TestBackups):
@@ -51,6 +52,35 @@ class TestBackupList(TestBackups):
         parsed_args = self.check_parser(self.cmd, [], [])
         columns, data = self.cmd.take_action(parsed_args)
         self.backup_client.list.assert_called_once_with(**self.defaults)
+        self.assertEqual(self.columns, columns)
+        self.assertEqual([self.values], data)
+
+
+class TestBackupListInstance(TestBackups):
+
+    defaults = {
+        'limit': None,
+        'marker': None
+    }
+
+    columns = database_backups.ListDatabaseInstanceBackups.columns
+    values = ('bk-1234', '1234', 'bkp_1', 'COMPLETED', None,
+              '2015-05-16T14:23:08')
+
+    def setUp(self):
+        super(TestBackupListInstance, self).setUp()
+        self.cmd = database_backups.ListDatabaseInstanceBackups(self.app, None)
+        data = [self.fake_backups.get_backup_bk_1234()]
+        self.instance_client.backups.return_value = common.Paginated(data)
+
+    @mock.patch.object(utils, 'find_resource')
+    def test_backup_list_defaults(self, mock_find):
+        args = ['1234']
+        mock_find.return_value = args[0]
+        parsed_args = self.check_parser(self.cmd, args, [])
+        columns, data = self.cmd.take_action(parsed_args)
+        self.instance_client.backups.assert_called_once_with('1234',
+                                                             **self.defaults)
         self.assertEqual(self.columns, columns)
         self.assertEqual([self.values], data)
 
