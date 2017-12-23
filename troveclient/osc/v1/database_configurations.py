481e600f35ee923a66c8bdb12389af2ddaf691d7
zhanggang <zhanggang@cmss.chinamobile.com>
2017-12-22 22:57:34 -0500

    Add configuration-create to OSC
    This change adds database support to the python-openstackclient
    project for the configuration-create command.
    
    The trove command configuration-create is now:
        openstack database configuration create
    
    Change-Id: I5aadb88860d0d69c8e2b4b78b5fb0dca2eba031f
    Partially-Implements: blueprint trove-support-in-python-openstackclient

--- troveclient/osc/v1/database_configurations.py
+++ troveclient/osc/v1/database_configurations.py
@@ -203,3 +203,53 @@ class DeleteDatabaseConfiguration(command.Command):
             msg = (_("Failed to delete configuration %(c_group)s: %(e)s")
                    % {'c_group': parsed_args.configuration_group, 'e': e})
             raise exceptions.CommandError(msg)
+
+
+class CreateDatabaseConfiguration(command.ShowOne):
+
+    _description = _("Creates a configuration group.")
+
+    def get_parser(self, prog_name):
+        parser = super(CreateDatabaseConfiguration, self).get_parser(prog_name)
+        parser.add_argument(
+            'name',
+            metavar='<name>',
+            help=_('Name of the configuration group.'),
+        )
+        parser.add_argument(
+            'values',
+            metavar='<values>',
+            help=_('Dictionary of the values to set.'),
+        )
+        parser.add_argument(
+            '--datastore',
+            metavar='<datastore>',
+            default=None,
+            help=_('Datastore assigned to the configuration group. Required '
+                   'if default datastore is not configured.'),
+        )
+        parser.add_argument(
+            '--datastore_version',
+            metavar='<datastore_version>',
+            default=None,
+            help=_('Datastore version ID assigned to the '
+                   'configuration group.'),
+        )
+        parser.add_argument(
+            '--description',
+            metavar='<description>',
+            default=None,
+            help=_('An optional description for the configuration group.'),
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        db_configurations = self.app.client_manager.database.configurations
+        config_grp = db_configurations.create(
+            parsed_args.name,
+            parsed_args.values,
+            description=parsed_args.description,
+            datastore=parsed_args.datastore,
+            datastore_version=parsed_args.datastore_version)
+        config_grp = set_attributes_for_print_detail(config_grp)
+        return zip(*sorted(six.iteritems(config_grp)))
