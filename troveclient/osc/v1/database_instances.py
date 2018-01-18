e89cadc0ed75b5fd42ff5b1ea123bb47d075b116
Zhao Chao <zhaochao1984@gmail.com>
2018-01-18 18:04:55 +0800

    Add force-delete to OSC
    This change adds database support for the python-openstackclient
    project for the force-delete and cluster-force-delete commands.
    
    The trove command force-delete is now:
        openstack database instance force delete
    The trove command cluster-force-delete is now:
        openstack database cluster force delete
    
    Change-Id: I919678ca6b06d3b5762eac1990cc1fbf0e149d7c
    Partially-Implements: blueprint trove-support-in-python-openstackclient
    Signed-off-by: Zhao Chao <zhaochao1984@gmail.com>

--- troveclient/osc/v1/database_instances.py
+++ troveclient/osc/v1/database_instances.py
@@ -475,3 +475,30 @@ class ResizeDatabaseInstanceVolume(command.Command):
         instance = osc_utils.find_resource(db_instances,
                                            parsed_args.instance)
         db_instances.resize_volume(instance, parsed_args.size)
+
+
+class ForceDeleteDatabaseInstance(command.Command):
+
+    _description = _("Force delete an instance.")
+
+    def get_parser(self, prog_name):
+        parser = (super(ForceDeleteDatabaseInstance, self)
+                  .get_parser(prog_name))
+        parser.add_argument(
+            'instance',
+            metavar='<instance>',
+            help=_('ID or name of the instance'),
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        db_instances = self.app.client_manager.database.instances
+        instance = osc_utils.find_resource(db_instances,
+                                           parsed_args.instance)
+        db_instances.reset_status(instance)
+        try:
+            db_instances.delete(instance)
+        except Exception as e:
+            msg = (_("Failed to delete instance %(instance)s: %(e)s")
+                   % {'instance': parsed_args.instance, 'e': e})
+            raise exceptions.CommandError(msg)
--- troveclient/tests/osc/v1/test_database_clusters.py
+++ troveclient/tests/osc/v1/test_database_clusters.py
@@ -205,3 +205,31 @@ class TestDatabaseClusterUpgrade(TestClusters):
         self.cluster_client.upgrade.assert_called_with('cluster1',
                                                        'datastore_version1')
         self.assertIsNone(result)
+
+
+class TestDatabaseClusterForceDelete(TestClusters):
+
+    def setUp(self):
+        super(TestDatabaseClusterForceDelete, self).setUp()
+        self.cmd = database_clusters.ForceDeleteDatabaseCluster(self.app, None)
+
+    @mock.patch.object(utils, 'find_resource')
+    def test_cluster_force_delete(self, mock_find):
+        args = ['cluster1']
+        mock_find.return_value = args[0]
+        parsed_args = self.check_parser(self.cmd, args, [])
+        result = self.cmd.take_action(parsed_args)
+        self.cluster_client.reset_status.assert_called_with('cluster1')
+        self.cluster_client.delete.assert_called_with('cluster1')
+        self.assertIsNone(result)
+
+    @mock.patch.object(utils, 'find_resource')
+    def test_cluster_force_delete_with_exception(self, mock_find):
+        args = ['fakecluster']
+        parsed_args = self.check_parser(self.cmd, args, [])
+        mock_find.return_value = args[0]
+
+        self.cluster_client.delete.side_effect = exceptions.CommandError
+        self.assertRaises(exceptions.CommandError,
+                          self.cmd.take_action,
+                          parsed_args)
