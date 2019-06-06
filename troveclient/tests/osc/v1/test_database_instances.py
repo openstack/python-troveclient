16d1dedb54dc70a6f0ae7e5068fd5582f0539a5a
Lingxian Kong <anlin.kong@gmail.com>
2019-06-06 13:11:35 +1200

    Support to batch delete database instances
    Allow users to delete database instances in batch, usage:
    
    usage: openstack database instance delete [-h] instance [instance ...]
    
    Change-Id: I88a15ba1910f7b3294067b54ae4a7a9b4c4a17a5

--- troveclient/tests/osc/v1/test_database_instances.py
+++ troveclient/tests/osc/v1/test_database_instances.py
@@ -14,6 +14,7 @@ import mock
 
 from osc_lib import exceptions
 from osc_lib import utils
+from oslo_utils import uuidutils
 
 from troveclient import common
 from troveclient.osc.v1 import database_instances
@@ -119,24 +120,43 @@ class TestDatabaseInstanceDelete(TestInstances):
         super(TestDatabaseInstanceDelete, self).setUp()
         self.cmd = database_instances.DeleteDatabaseInstance(self.app, None)
 
-    @mock.patch.object(utils, 'find_resource')
-    def test_instance_delete(self, mock_find):
-        args = ['instance1']
-        mock_find.return_value = args[0]
-        parsed_args = self.check_parser(self.cmd, args, [])
-        result = self.cmd.take_action(parsed_args)
-        self.instance_client.delete.assert_called_with('instance1')
-        self.assertIsNone(result)
+    @mock.patch("troveclient.utils.get_resource_id_by_name")
+    def test_instance_delete(self, mock_getid):
+        mock_getid.return_value = "fake_uuid"
 
-    @mock.patch.object(utils, 'find_resource')
-    def test_instance_delete_with_exception(self, mock_find):
+        args = ['instance1']
+        parsed_args = self.check_parser(self.cmd, args, [])
+        self.cmd.take_action(parsed_args)
+
+        mock_getid.assert_called_once_with(self.instance_client, "instance1")
+        self.instance_client.delete.assert_called_with('fake_uuid')
+
+    @mock.patch("troveclient.utils.get_resource_id_by_name")
+    def test_instance_delete_with_exception(self, mock_getid):
+        mock_getid.side_effect = exceptions.CommandError
+
         args = ['fakeinstance']
         parsed_args = self.check_parser(self.cmd, args, [])
 
-        mock_find.side_effect = exceptions.CommandError
         self.assertRaises(exceptions.CommandError,
                           self.cmd.take_action,
                           parsed_args)
+
+    @mock.patch("troveclient.utils.get_resource_id_by_name")
+    def test_instance_bulk_delete(self, mock_getid):
+        instance_1 = uuidutils.generate_uuid()
+        instance_2 = uuidutils.generate_uuid()
+        mock_getid.return_value = instance_1
+
+        args = ["fake_instance", instance_2]
+        parsed_args = self.check_parser(self.cmd, args, [])
+        self.cmd.take_action(parsed_args)
+
+        mock_getid.assert_called_once_with(self.instance_client,
+                                           "fake_instance")
+
+        calls = [mock.call(instance_1), mock.call(instance_2)]
+        self.instance_client.delete.assert_has_calls(calls)
 
 
 class TestDatabaseInstanceCreate(TestInstances):
